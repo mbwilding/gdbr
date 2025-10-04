b978b97fb6b65313ac139dd28bc4c24f8081081c
Matthew Wilding <mbwilding@gmail.com>
2025-10-05 05:10:05 +0800

    update

--- src/tabs.rs
+++ src/tabs.rs
@@ -90,7 +90,7 @@ impl TabViewer for Tabs {
                                 .font(TextStyle::Monospace),
                         );
 
-                        if response.lost_focus() {
+                        if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                             response.request_focus();
                             // TODO: Handle command entered
                             self.logs.push_str(&self.console_input);
