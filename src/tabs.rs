2d3ed91dbe57619453fd8b9433098885a0c6500c
Matthew Wilding <mbwilding@gmail.com>
2025-10-05 05:19:16 +0800

    update

--- src/tabs.rs
+++ src/tabs.rs
@@ -45,6 +45,7 @@ impl Tab {
 #[derive(Debug, Default)]
 pub struct Tabs {
     console_input: String,
+    console_input_prev: String,
     logs: String,
 }
 
@@ -93,11 +94,16 @@ impl TabViewer for Tabs {
                         if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                             response.request_focus();
                             if self.console_input.is_empty() {
-                                // TODO: enter = continue stepping the debugger
+                                // TODO: enter = repeat last
+                                self.logs.push_str(&self.console_input_prev);
+                                self.logs.push('\n');
                             } else {
                                 // TODO: post to gdb instead
                                 self.logs.push_str(&self.console_input);
                                 self.logs.push('\n');
+                                if self.console_input_prev != self.console_input {
+                                    self.console_input_prev = self.console_input.clone();
+                                }
                                 self.console_input.clear();
                             }
                         };
