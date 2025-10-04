402d11dd370a27bcc38a4b8040cc5eb97745b1e6
Matthew Wilding <mbwilding@gmail.com>
2025-10-04 20:25:47 +0800

    Update

---
+++ src/app.rs
@@ -0,0 +1,148 @@
+use egui::{CentralPanel, Color32, Frame, MenuBar, TopBottomPanel};
+use egui_dock::{DockArea, DockState, NodeIndex, Style};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(default)]
+pub struct Gdbr {
+    pub dock_state: DockState<String>,
+}
+
+fn reset_dock() -> DockState<String> {
+    let mut dock_state = DockState::new(vec!["tab1".to_owned(), "tab2".to_owned()]);
+
+    let [left, right] =
+        dock_state
+            .main_surface_mut()
+            .split_left(NodeIndex::root(), 0.3, vec!["tab3".to_owned()]);
+
+    let [_, _] = dock_state
+        .main_surface_mut()
+        .split_below(left, 0.7, vec!["tab4".to_owned()]);
+
+    let [_, _] = dock_state
+        .main_surface_mut()
+        .split_below(right, 0.5, vec!["tab5".to_owned()]);
+
+    dock_state
+}
+
+impl Default for Gdbr {
+    fn default() -> Self {
+        Self {
+            dock_state: reset_dock(),
+        }
+    }
+}
+
+impl Gdbr {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        cc.egui_ctx.set_visuals(egui::Visuals {
+            dark_mode: true,
+            // Foreground color
+            override_text_color: Some(Color32::WHITE),
+            // Background color
+            panel_fill: Color32::BLACK,
+            // Button frame
+            button_frame: true,
+            ..Default::default()
+        });
+
+        let mut fonts = egui::FontDefinitions::default();
+
+        fonts.font_data.insert(
+            "NeoSpleen".to_owned(),
+            #[expect(clippy::large_include_file)]
+            std::sync::Arc::new(egui::FontData::from_static(include_bytes!(
+                "../assets/NeoSpleenNerdFont-Regular.ttf"
+            ))),
+        );
+
+        // fonts
+        //     .families
+        //     .entry(egui::FontFamily::Proportional)
+        //     .or_default()
+        //     .insert(0, "NeoSpleen".to_owned());
+
+        fonts
+            .families
+            .entry(egui::FontFamily::Monospace)
+            .or_default()
+            .insert(0, "NeoSpleen".to_owned());
+
+        cc.egui_ctx.set_fonts(fonts);
+
+        if let Some(storage) = cc.storage {
+            eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
+        } else {
+            Default::default()
+        }
+    }
+}
+
+struct TabViewer {
+    modified: bool,
+}
+
+impl Default for TabViewer {
+    fn default() -> Self {
+        Self { modified: false }
+    }
+}
+
+impl egui_dock::TabViewer for TabViewer {
+    type Tab = String;
+
+    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
+        (&*tab).into()
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
+        ui.label(format!("Content of {tab}"));
+    }
+
+    fn on_rect_changed(&mut self, _tab: &mut Self::Tab) {
+        self.modified = true
+    }
+}
+
+impl eframe::App for Gdbr {
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Tabs
+        let mut tab_viewer = TabViewer::default();
+        DockArea::new(&mut self.dock_state)
+            .style(Style::from_egui(ctx.style().as_ref()))
+            .show(ctx, &mut tab_viewer);
+
+        // Top bar
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            MenuBar::new().ui(ui, |ui| {
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    if ui.button("Reset Dock").clicked() {
+                        self.dock_state = reset_dock();
+                    }
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        // Central tab viewer
+        CentralPanel::default()
+            .frame(Frame::central_panel(&ctx.style()).inner_margin(0.))
+            .show(ctx, |ui| {
+                DockArea::new(&mut self.dock_state).show_inside(ui, &mut tab_viewer);
+            });
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![warn(clippy::all, rust_2018_idioms)]
+
+mod app;
+mod gdbr;
+mod structs;
+pub use app::Gdbr;
