add5353e08d8119985149048b4bcab43812ae25a
Matthew Wilding <mbwilding@gmail.com>
2025-10-05 05:13:36 +0800

    update

--- src/tabs.rs
+++ src/tabs.rs
@@ -92,10 +92,14 @@ impl TabViewer for Tabs {
 
                         if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                             response.request_focus();
-                            // TODO: Handle command entered
-                            self.logs.push_str(&self.console_input);
-                            self.logs.push('\n');
-                            self.console_input.clear();
+                            if self.console_input.is_empty() {
+                                // TODO: enter = continue stepping the debugger
+                            } else {
+                                // TODO: post to gdb instead
+                                self.logs.push_str(&self.console_input);
+                                self.logs.push('\n');
+                                self.console_input.clear();
+                            }
                         };
                     });
                 });
